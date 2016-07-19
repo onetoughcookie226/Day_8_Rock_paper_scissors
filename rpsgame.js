b508b82de2991f8ff9de445a9b26e2baf57ffa3a
onetoughcookie226 <onetoughcookie226@gmail.com>
2016-07-19 22:57:33 +0200

    Animation effects

--- rpsgame.js
+++ rpsgame.js
@@ -1,6 +1,27 @@
 $(document).ready(function () {
     console.log("i am about to shake");
-    $("h1").addClass("animated bounce");
+
+
+    function myMove () {
+
+        var elem = document.getElementById("myAnimation");
+        var pos = 0;
+        var id = setInterval(frame, 10);
+        function frame() {
+            if (pos == 350) {
+                clearInterval(id);
+            } else {
+                /* code to change the element style */
+                pos++;
+                elem.style.top = pos + 'px';
+                elem.style.left = pos + 'px';
+            }
+        }
+
+    }
+
+
+
 
 });
 
