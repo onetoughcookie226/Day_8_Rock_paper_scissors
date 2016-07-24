2918ba77830e74bb5edf590c2a10e9e32a267cca
onetoughcookie226 <onetoughcookie226@gmail.com>
2016-07-24 20:24:16 +0200

    Add shake with jquery click event

--- rpsgame.js
+++ rpsgame.js
@@ -1,27 +1,37 @@
 $(document).ready(function () {
-    console.log("i am about to shake");
+    console.log("i am about to shake 1");
+
+    function myMove() {
+        $("#left-fist").addClass("animated shake") ;
+    };
+
+    $("#rock").click(function() {
+        myMove();
+    });
 
 
-    function myMove () {
 
+  /*  $("#rock").click(function myMove () {
+     console.log("my move was called");
         var elem = document.getElementById("myAnimation");
         var pos = 0;
         var id = setInterval(frame, 10);
         function frame() {
+            console.log("tick");
             if (pos == 350) {
                 clearInterval(id);
             } else {
                 /* code to change the element style */
-                pos++;
+            /*    pos++;
                 elem.style.top = pos + 'px';
                 elem.style.left = pos + 'px';
             }
         }
-
-    }
-
-
-
+    }); */
 
 });
 
+function myFunction () {
+
+    console.log("test");
+}
