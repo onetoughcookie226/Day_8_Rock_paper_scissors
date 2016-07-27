eed1c1aa8bb5151fa3a70a77e5457d4ee4703599
onetoughcookie226 <onetoughcookie226@gmail.com>
2016-07-27 11:23:33 +0200

    Display paper after clicking the paper buttun

--- rpsgame.js
+++ rpsgame.js
@@ -1,51 +1,38 @@
-//shake fists after clicking the buttons"
-
-var openFist = ["rock", "paper", "scissors"];
-
 $(document).ready(function () {
     console.log("i am about to shake the left-fist");
+    //arrays of weapens
+    var weapons = ['rock', 'paper', 'scissors'];
 
+        //Apply animated shake class to the fists. declare the functions
     function leftFistShake() {
         $("#left-fist").addClass("animated shake") ;
     };
     function rightFistShake() {
-       $("#right-fist").addClass("animated shake");
+        $("#right-fist").addClass("animated shake");
     };
-
-  $( function () {
-
-    });
-
+    //click on buttons and both fists shake
     $(".buttons").click(function() {
         leftFistShake.call(this);
         rightFistShake.call(this);
     });
+    // if you select rock, right-fist will shake and display rock
 
+    function rightFistResultPaper() {
+        $("#result-right-paper").css('display', 'inline-block');
+    }
 
+    $("#paper").click(function() {
+        rightFistResultPaper()
+    });
+    
+//right fist show the selected weapon
+//left fist randomly display a weapon
+//logic part
+//keep the score
+// show game over after 3 times lost
+
+//shake fists after clicking the buttons"
 
     //keep scores of the loses.
 
-  /*  $("#rock").click(function myMove () {
-     console.log("my move was called");
-        var elem = document.getElementById("myAnimation");
-        var pos = 0;
-        var id = setInterval(frame, 10);
-        function frame() {
-            console.log("tick");
-            if (pos == 350) {
-                clearInterval(id);
-            } else {
-                /* code to change the element style */
-            /*    pos++;
-                elem.style.top = pos + 'px';
-                elem.style.left = pos + 'px';
-            }
-        }
-    }); */
-
 });
-
-function myFunction () {
-
-    console.log("test");
-}
